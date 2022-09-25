fac88d0fc0d488e31f35ba9b60366c3f735f6a5e
dlrjsals97 <kmlee97s@gmail.com>
2022-09-25 15:37:49 +0900

    feat: React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBU9t89FUmHdOLd0RURKznW47ashS49i8c",
+  authDomain: "nwitter-810c8.firebaseapp.com",
+  projectId: "nwitter-810c8",
+  storageBucket: "nwitter-810c8.appspot.com",
+  messagingSenderId: "681832958178",
+  appId: "1:681832958178:web:3304942104aab3ab8176bb"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

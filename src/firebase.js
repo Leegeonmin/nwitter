3b0d39a67830570f1edc4577545cfef64b58527c
dlrjsals97 <kmlee97s@gmail.com>
2022-09-25 16:12:39 +0900

    Docs: git ignore 추가 , Securing keys

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import firebase from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBU9t89FUmHdOLd0RURKznW47ashS49i8c",
-  authDomain: "nwitter-810c8.firebaseapp.com",
-  projectId: "nwitter-810c8",
-  storageBucket: "nwitter-810c8.appspot.com",
-  messagingSenderId: "681832958178",
-  appId: "1:681832958178:web:3304942104aab3ab8176bb"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase

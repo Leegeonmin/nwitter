4d48651eab94f2d928d4e1724751b197a47b7fcc
dlrjsals97 <kmlee97s@gmail.com>
2022-10-11 01:48:04 +0900

    chore: Prettier 적용으로 인한 코드 일부 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
@@ -10,7 +10,7 @@ const AppRouter = ({ isLoggedIn }) => {
         {isLoggedIn ? (
           <>
             <Route exact path="/">
-              <Home />{" "}
+              <Home />
             </Route>
           </>
         ) : (

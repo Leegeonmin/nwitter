bb638bbee7d62d705e10ed6955fefa68cf341ff7
dlrjsals97 <kmlee97s@gmail.com>
2022-10-11 20:15:09 +0900

    feat: 로그인시 로그아웃 버튼 생성

---
+++ src/components/CustomButton.js
@@ -0,0 +1,3 @@
+export const CustomButton = ({ onClickFunction, innerText }) => (
+  <button onClick={onClickFunction}>{innerText}</button>
+);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { authService } from "FbInstance";
+import { CustomButton } from "../components/CustomButton";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const signOutOnClick = async () => await signOut(authService);
+  const inText = "Log Out";
+
+  return (
+    <>
+      <span>Home</span>
+      <CustomButton onClickFunction={signOutOnClick} innerText={inText} />
+    </>
+  );
+};
 export default Home;

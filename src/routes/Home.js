ed58352e8d908c093dd018346a0f12327efda70c
dlrjsals97 <kmlee97s@gmail.com>
2022-10-13 02:23:27 +0900

    feat: Create, RealTime Read기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,32 +1,31 @@
 import { dbService } from "FbInstance";
 import { query } from "firebase/database";
-import { addDoc, collection, doc, getDocs } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, orderBy } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const getNweets = async () => {
-    const dbQuery = await query(collection(dbService, "nweets"));
-    const querySnapshot = await getDocs(dbQuery);
-    querySnapshot.forEach((e) => {
-      const nweetObj = {
-        ...e.data(),
-        id: e.id,
-      };
-      setNweets((prev) => [nweetObj, ...prev]);
-    });
-  };
   useEffect(() => {
-    setNweets([]);
-    getNweets();
+    const dbQuery = query(
+      collection(dbService, "nweets"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(dbQuery, (querySnapshot) => {
+      const nweetArray = querySnapshot.docs.map((e) => ({
+        id: e.id,
+        ...e.data(),
+      }));
+      setNweets((prev) => [prev, ...nweetArray]);
+    });
   }, []);
+
   const onSubmit = async (event) => {
     event.preventDefault();
-    const data = await addDoc(collection(dbService, "nweets"), {
-      nweet: nweet,
+    await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
-    console.log(data);
   };
   const onChange = (event) => {
     setNweet(event.target.value);
@@ -49,7 +48,7 @@ const Home = () => {
         {nweets.map((nweet) => {
           return (
             <div key={nweet.id}>
-              <h4>{nweet.nweet}</h4>
+              <h4>{nweet.text}</h4>
             </div>
           );
         })}
